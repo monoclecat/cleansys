6fcc27cfd3aa91ade3ef2c0ef3d574cfaba6ad24
Andy Delay <signup@andrewdelay.com>
2018-01-13 19:35:39 +0100

    Updated to Django 2.0

--- webinterface/urls.py
+++ webinterface/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url
+from django.urls import path
 from django.contrib.auth.decorators import login_required
 from .views import *
 
@@ -8,58 +9,60 @@ import datetime
 
 app_name = 'webinterface'
 urlpatterns = [
-    url(r'^$', WelcomeView.as_view(), name='welcome'),
+    path('', WelcomeView.as_view(), name='welcome'),
+    #url(r'^$', WelcomeView.as_view(), name='welcome'),
 
-    url(r'^switch/(?P<pk>[\d]+)/(?P<answer>[\S]+)/$', DutySwitchView.as_view(), name='switch-duty-answer'),
-    url(r'^switch/(?P<pk>[\d]+)/$', DutySwitchView.as_view(), name='switch-duty'),
+    #url(r'^switch/(?P<pk>[\d]+)/(?P<answer>[\S]+)/$', DutySwitchView.as_view(), name='switch-duty-answer'),
+    path('switch/<int:pk>/<answer>/', DutySwitchView.as_view(), name='switch-duty-answer'),
+    path('switch/<int:pk>/', DutySwitchView.as_view(), name='switch-duty'),
 
-    url(r'^clean/(?P<duty_pk>[\d]+)/(?P<cleaner_pk>[\d]+)/$', DutyCleanView.as_view(), name='clean-duty'),
+    path('^clean/<int:duty_pk>/<int:cleaner_pk>/', DutyCleanView.as_view(), name='clean-duty'),
 
-    url(r'^putzer/(?P<slug>[\S]+)/(?P<page>[\d]+)/$', CleanerView.as_view(), name='cleaner-duties'),
+    path('putzer/<slug:slug>/<int:page>/', CleanerView.as_view(), name='cleaner-duties'),
 
-    url(r'^schedule/all/(?P<page>[\d]+)/(?P<cleaner_pk>[\d]+)/$', CleaningScheduleView.as_view(),
-        name='all-schedule-view-highlight'),
-    url(r'^schedule/all/(?P<page>[\d]+)/$', CleaningScheduleView.as_view(), name='all-schedule-view'),
+    path('schedule/all/<int:page>/<int:cleaner_pk>/', CleaningScheduleView.as_view(),
+         name='all-schedule-view-highlight'),
+    path('schedule/all/<int:page>/', CleaningScheduleView.as_view(), name='all-schedule-view'),
 
-    url(r'^schedule/(?P<pk>[\d]+)/(?P<page>[\d]+)/(?P<cleaner_pk>[\d]+)/$', CleaningScheduleView.as_view(),
-        name='schedule-view-highlight'),
-    url(r'^schedule/(?P<pk>[\d]+)/(?P<page>[\d]+)/$', CleaningScheduleView.as_view(), name='schedule-view'),
+    path('schedule/<int:pk>/<int:page>/<int:cleaner_pk>/', CleaningScheduleView.as_view(),
+         name='schedule-view-highlight'),
+    path('schedule/<int:pk>/<int:page>/', CleaningScheduleView.as_view(), name='schedule-view'),
 
-    url(r'^config/$', login_required(ConfigView.as_view()), name='config'),
-    url(r'^results/$', RedirectView.as_view(
-        url=reverse_lazy('webinterface:results',
+    path('config/', login_required(ConfigView.as_view()), name='config'),
+    path('results/', RedirectView.as_view(
+         url=reverse_lazy('webinterface:results',
                     kwargs={'from_day': datetime.datetime.now().day, 'from_month': datetime.datetime.now().month,
                             'from_year': datetime.datetime.now().year,
                             'to_day': (datetime.datetime.now() + datetime.timedelta(days=30)).day,
                             'to_month': (datetime.datetime.now() + datetime.timedelta(days=30)).month,
                             'to_year': (datetime.datetime.now() + datetime.timedelta(days=30)).year})),
-        name='results-now'),
+         name='results-now'),
 
-    url(r'^results/(?P<from_day>[\d]+)-(?P<from_month>[\d]+)-(?P<from_year>[\d]+)/'
-        r'(?P<to_day>[\d]+)-(?P<to_month>[\d]+)-(?P<to_year>[\d]+)/(?P<options>[\w]+)$',
-        login_required(ResultsView.as_view()), name='results'),
-    url(r'^results/(?P<from_day>[\d]+)-(?P<from_month>[\d]+)-(?P<from_year>[\d]+)/'
-        r'(?P<to_day>[\d]+)-(?P<to_month>[\d]+)-(?P<to_year>[\d]+)$',
-        login_required(ResultsView.as_view()), name='results'),
+    path('results/<int:from_day>-<int:from_month>-<int:from_year>/'
+         '<int:to_day>-<int:to_month>-<int:to_year>/<options>/',
+         login_required(ResultsView.as_view()), name='results'),
+    path('results/<int:from_day>-<int:from_month>-<int:from_year>/'
+         '<int:to_day>-<int:to_month>-<int:to_year>',
+         login_required(ResultsView.as_view()), name='results'),
 
-    url(r'^cleaner-new/$', login_required(CleanerNewView.as_view()), name='cleaner-new'),
-    url(r'^cleaner-edit/(?P<pk>[\d]+)/$', login_required(CleanerUpdateView.as_view()),
-        name='cleaner-edit'),
-    url(r'^cleaner-delete/(?P<pk>[\d]+)/$', login_required(CleanerDeleteView.as_view()), name='cleaner-delete'),
+    path('cleaner-new/', login_required(CleanerNewView.as_view()), name='cleaner-new'),
+    path('cleaner-edit/<int:pk>/', login_required(CleanerUpdateView.as_view()),
+         name='cleaner-edit'),
+    path('cleaner-delete/<int:pk>/', login_required(CleanerDeleteView.as_view()), name='cleaner-delete'),
 
-    url(r'^cleaning-schedule-new/$', login_required(CleaningScheduleNewView.as_view()), name='cleaning-schedule-new'),
-    url(r'^cleaning-schedule-edit/(?P<pk>[\d]+)/$', login_required(CleaningScheduleUpdateView.as_view()),
-        name='cleaning-schedule-edit'),
-    url(r'^cleaning-schedule-delete/(?P<pk>[\d]+)/$', login_required(CleaningScheduleDeleteView.as_view()),
-        name='cleaning-schedule-delete'),
+    path('cleaning-schedule-new/', login_required(CleaningScheduleNewView.as_view()), name='cleaning-schedule-new'),
+    path('cleaning-schedule-edit/<int:pk>/', login_required(CleaningScheduleUpdateView.as_view()),
+         name='cleaning-schedule-edit'),
+    path('cleaning-schedule-delete/<int:pk>/', login_required(CleaningScheduleDeleteView.as_view()),
+         name='cleaning-schedule-delete'),
 
-    url(r'^cleaning-schedule-group-new/$', login_required(CleaningScheduleGroupNewView.as_view()),
-        name='cleaning-schedule-group-new'),
-    url(r'^cleaning-schedule-group-edit/(?P<pk>[\d]+)/$', login_required(CleaningScheduleGroupUpdateView.as_view()),
-        name='cleaning-schedule-group-edit'),
-    url(r'^cleaning-schedule-group-delete/(?P<pk>[\d]+)/$', login_required(CleaningScheduleGroupDeleteView.as_view()),
-        name='cleaning-schedule-group-delete'),
+    path('cleaning-schedule-group-new/', login_required(CleaningScheduleGroupNewView.as_view()),
+         name='cleaning-schedule-group-new'),
+    path('cleaning-schedule-group-edit/<int:pk>/', login_required(CleaningScheduleGroupUpdateView.as_view()),
+         name='cleaning-schedule-group-edit'),
+    path('cleaning-schedule-group-delete/<int:pk>/', login_required(CleaningScheduleGroupDeleteView.as_view()),
+         name='cleaning-schedule-group-delete'),
 
-    url(r'^login/$', login_view, name='login'),
-    url(r'^logout/$', login_required(login_view), name='logout'),
+    path('login/', login_view, name='login'),
+    path('logout/', login_required(login_view), name='logout'),
 ]
