8d371192934c553e73b20c98d122401956182dc7
Andy <signup@andrewdelay.com>
2020-03-30 13:14:24 +0200

    Better grouping and ordering of url paths

--- webinterface/urls.py
+++ webinterface/urls.py
@@ -11,16 +11,20 @@ from django.views.generic.base import RedirectView
 app_name = 'webinterface'
 urlpatterns = [
     path('', RedirectView.as_view(url=reverse_lazy("webinterface:cleaner-no-page")), name='welcome'),
+    path('login/', LoginView.as_view(template_name="webinterface/generic_form.html", extra_context={'title': "Login"},
+                                     authentication_form=AuthFormWithSubmit), name='login'),
+    path('login-per-klick', LoginByClickView.as_view(), name='login-by-click'),
+    path('logout/', login_required(LogoutView.as_view()), name='logout'),
+    path('config/', staff_member_required(ConfigView.as_view(), login_url=reverse_lazy("webinterface:login")),
+         name='config'),
 
-    #url(r'^switch/(?P<pk>[\d]+)/(?P<answer>[\S]+)/$', DutySwitchView.as_view(), name='switch-duty-answer'),
-    path('tauschen/<int:assignment_pk>/<int:page>', login_required(DutySwitchNewView.as_view()),
-         name='dutyswitch-create'),
-
-    path('tauschanfrage-akzeptieren/<int:pk>/<int:page>', login_required(DutySwitchUpdateView.as_view()),
-         name='dutyswitch-accept'),
-
-    path('tauschanfrage-loeschen/<int:pk>/<int:page>', login_required(DutySwitchDeleteView.as_view()),
-         name='dutyswitch-delete'),
+    path('affiliation-edit/<int:pk>/', staff_member_required(AffiliationUpdateView.as_view()), name='affiliation-edit'),
+    path('affiliation-delete/<int:pk>/', staff_member_required(AffiliationDeleteView.as_view()),
+         name='affiliation-delete'),
+    path('putzer/<int:pk>/zugehoerigkeiten/', login_required(AffiliationNewView.as_view()),
+         name='affiliation-list'),
+    path('putzer/<int:pk>/zugehoerigkeiten/p<int:cleaner_page>/', login_required(AffiliationNewView.as_view()),
+         name='affiliation-list-with-cleaner-page'),
 
     path('putzen/<int:cleaning_week_pk>/', login_required(AssignmentTasksView.as_view()),
          name='assignment-tasks'),
@@ -28,16 +32,40 @@ urlpatterns = [
          name='assignment-tasks-back-to-schedule'),
     path('putzen/<int:cleaning_week_pk>/p<int:cleaner_page>/', login_required(AssignmentTasksView.as_view()),
          name='assignment-tasks-back-to-cleaner'),
+    path('assignment-create/<int:schedule_pk>/<int:page>/', staff_member_required(AssignmentCreateView.as_view()),
+         name='assignment-create-back-to-schedule'),
+    path('assignment-create/<int:schedule_pk>/<int:page>/<int:initial_begin>/<int:initial_end>/',
+         staff_member_required(AssignmentCreateView.as_view()),
+         name='assignment-create-init'),
+    path('assignment-edit/<int:pk>/<int:page>/', staff_member_required(AssignmentUpdateView.as_view()),
+         name='assignment-edit'),
 
-    path('geputzt/<int:assignment_pk>/<int:task_pk>', login_required(TaskCleanedView.as_view()),
-         name='task-cleaned'),
-
-    # path('putzer/<slug:slug>/seite<int:page>/', CleanerView.as_view(), name='cleaner'),
     path('du/seite<int:page>/', login_required(CleanerView.as_view()), name='cleaner'),
     path('du/', login_required(CleanerView.as_view()), name='cleaner-no-page'),
+    path('putzer-analytics/', login_required(CleanerAnalyticsView.as_view()), name='cleaner-analytics'),
+    path('putzer-analytics/p<int:cleaner_page>/', login_required(CleanerAnalyticsView.as_view()),
+         name='cleaner-analytics-with-cleaner-page'),
+
+    path('putzer/neu/', staff_member_required(CleanerNewView.as_view()), name='cleaner-new'),
+    path('putzer/<int:pk>/', staff_member_required(CleanerUpdateView.as_view()),
+         name='cleaner-edit'),
+    path('cleaner-delete/<int:pk>/', staff_member_required(CleanerDeleteView.as_view()), name='cleaner-delete'),
+
+    path('cleaning-week-tasks/<int:pk>/<int:page>/', staff_member_required(TaskCreateView.as_view()),
+         name='cleaning-week-tasks'),
+    path('cleaning-week-edit/<int:pk>/<int:page>/', staff_member_required(CleaningWeekUpdateView.as_view()),
+         name='cleaning-week-edit'),
+    path('cleaning-week-delete/<int:pk>/<int:page>/', staff_member_required(CleaningWeekDeleteView.as_view()),
+         name='cleaning-week-delete'),
+
+    path('tauschen/<int:assignment_pk>/<int:page>', login_required(DutySwitchNewView.as_view()),
+         name='dutyswitch-create'),
+    path('tauschanfrage-akzeptieren/<int:pk>/<int:page>', login_required(DutySwitchUpdateView.as_view()),
+         name='dutyswitch-accept'),
+    path('tauschanfrage-loeschen/<int:pk>/<int:page>', login_required(DutySwitchDeleteView.as_view()),
+         name='dutyswitch-delete'),
 
     path('putzplan-liste/', login_required(ScheduleList.as_view()), name='schedule-list'),
-
     path('putzplan/<slug:slug>/ab<int:week>/druckansicht/', login_required(SchedulePrintView.as_view()),
          name='schedule-print-view'),
     # path('putzplan/<slug:slug>/seite<int:page>/<slug:highlight_slug>/', login_required(ScheduleView.as_view()),
@@ -48,30 +76,6 @@ urlpatterns = [
     path('putzplan/<slug:slug>/analytics/s<int:schedule_page>/', login_required(ScheduleAnalyticsView.as_view()),
          name='schedule-analystics-view-with-schedule-page'),
     path('putzplan/<slug:slug>/', login_required(ScheduleView.as_view()), name='schedule-no-page'),
-
-
-    path('config/', staff_member_required(ConfigView.as_view(), login_url=reverse_lazy("webinterface:login")), name='config'),
-    path('putzer-analytics/', login_required(CleanerAnalyticsView.as_view()), name='cleaner-analytics'),
-    path('putzer-analytics/p<int:cleaner_page>/', login_required(CleanerAnalyticsView.as_view()),
-         name='cleaner-analytics-with-cleaner-page'),
-
-    path('cleaning-week-tasks/<int:pk>/<int:page>/', staff_member_required(TaskCreateView.as_view()),
-         name='cleaning-week-tasks'),
-
-    path('cleaning-week-edit/<int:pk>/<int:page>/', staff_member_required(CleaningWeekUpdateView.as_view()),
-         name='cleaning-week-edit'),
-
-    path('cleaning-week-delete/<int:pk>/<int:page>/', staff_member_required(CleaningWeekDeleteView.as_view()),
-         name='cleaning-week-delete'),
-
-    path('assignment-create/<int:schedule_pk>/<int:page>/', staff_member_required(AssignmentCreateView.as_view()),
-         name='assignment-create-back-to-schedule'),
-    path('assignment-create/<int:schedule_pk>/<int:page>/<int:initial_begin>/<int:initial_end>/',
-         staff_member_required(AssignmentCreateView.as_view()),
-         name='assignment-create-init'),
-
-    path('assignment-edit/<int:pk>/<int:page>/', staff_member_required(AssignmentUpdateView.as_view()),
-         name='assignment-edit'),
 
     path('schedule-new/', staff_member_required(ScheduleNewView.as_view()), name='schedule-new'),
     path('schedule-edit/<int:pk>/', staff_member_required(ScheduleUpdateView.as_view()),
@@ -90,27 +94,8 @@ urlpatterns = [
     path('schedule-group-delete/<int:pk>/', staff_member_required(ScheduleGroupDeleteView.as_view()),
          name='schedule-group-delete'),
 
-    path('putzer/neu/', staff_member_required(CleanerNewView.as_view()), name='cleaner-new'),
-    path('putzer/<int:pk>/', staff_member_required(CleanerUpdateView.as_view()),
-         name='cleaner-edit'),
-    path('putzer/<int:pk>/zugehoerigkeiten/', login_required(AffiliationNewView.as_view()),
-         name='affiliation-list'),
-    path('putzer/<int:pk>/zugehoerigkeiten/p<int:cleaner_page>/', login_required(AffiliationNewView.as_view()),
-         name='affiliation-list-with-cleaner-page'),
-
-    path('cleaner-delete/<int:pk>/', staff_member_required(CleanerDeleteView.as_view()), name='cleaner-delete'),
-
-    path('affiliation-edit/<int:pk>/', staff_member_required(AffiliationUpdateView.as_view()), name='affiliation-edit'),
-
-    path('affiliation-delete/<int:pk>/', staff_member_required(AffiliationDeleteView.as_view()),
-         name='affiliation-delete'),
-
+    path('geputzt/<int:assignment_pk>/<int:task_pk>', login_required(TaskCleanedView.as_view()),
+         name='task-cleaned'),
     path('task-edit/<int:pk>/', staff_member_required(TaskTemplateUpdateView.as_view()), name='task-edit'),
     path('task-delete/<int:pk>/', staff_member_required(TaskTemplateDeleteView.as_view()), name='task-delete'),
-
-
-    path('login/', LoginView.as_view(template_name="webinterface/generic_form.html", extra_context={'title': "Login"},
-                                     authentication_form=AuthFormWithSubmit), name='login'),
-    path('login-per-klick', LoginByClickView.as_view(), name='login-by-click'),
-    path('logout/', login_required(LogoutView.as_view()), name='logout'),
 ]
