a5ae8c187c8826f45869ba80b25af67e935b3f93
Andy <signup@andrewdelay.com>
2020-04-01 15:42:59 +0200

    Filled coverage holes in TaskTemplate unit tests

--- webinterface/tests/unit_tests/test_model_TaskTemplate.py
+++ webinterface/tests/unit_tests/test_model_TaskTemplate.py
@@ -1,17 +1,23 @@
 from django.test import TestCase
 from webinterface.models import *
 
-import logging
 from unittest.mock import *
 
 
 class TaskTemplateTest(TestCase):
-    @classmethod
-    def setUpTestData(cls):
-        cls.week = 2500
-        cls.schedule = Schedule.objects.create(name="schedule", weekday=3)
-        cls.cleaning_week = CleaningWeek.objects.create(schedule=cls.schedule, week=cls.week)
-        cls.task_template = TaskTemplate.objects.create(schedule=cls.schedule, start_days_before=1, end_days_after=1)
+    def setUp(self):
+        self.start_week = 2500
+        self.schedule = Schedule.objects.create(name="schedule", weekday=3)
+        self.cw1 = CleaningWeek.objects.create(schedule=self.schedule, week=self.start_week)
+        self.cw2 = CleaningWeek.objects.create(schedule=self.schedule, week=self.start_week+1)
+        self.cw3 = CleaningWeek.objects.create(schedule=self.schedule, week=self.start_week+2)
+
+        self.task_template = TaskTemplate.objects.create(
+            task_name='task1', schedule=self.schedule, start_days_before=1, end_days_after=1)
+
+        self.task_cw1 = Task.objects.create(cleaning_week=self.cw1, template=self.task_template)
+        self.task_cw2 = Task.objects.create(cleaning_week=self.cw2, template=self.task_template)
+        self.task_cw3 = Task.objects.create(cleaning_week=self.cw3, template=self.task_template)
 
     def test__str(self):
         self.assertEqual(self.task_template.__str__(), self.task_template.task_name)
@@ -21,3 +27,15 @@ class TaskTemplateTest(TestCase):
 
     def test__end_day_to_weekday(self):
         self.assertEqual(self.task_template.end_day_to_weekday(), Schedule.WEEKDAYS[4][1])
+
+    @patch('webinterface.models.current_epoch_week', autospec=True)
+    @patch('webinterface.models.CleaningWeek.create_missing_tasks', autospec=True)
+    def test__creation_triggers_task_creation(self, mock_create_missing, mock_current_epoch_week):
+        mock_current_epoch_week.return_value = self.start_week
+        TaskTemplate.objects.create(task_name='task2', schedule=self.schedule, start_days_before=1, end_days_after=1)
+        self.assertSetEqual(set(x[0][0] for x in mock_create_missing.call_args_list),
+                            {self.cw2, self.cw3})
+
+    def test__deletion_cascades_to_tasks(self):
+        self.task_template.delete()
+        self.assertFalse(Task.objects.exists())
