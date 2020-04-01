a718e80447701cbd51df96c4c9895ef50ebb807f
Andy <signup@andrewdelay.com>
2020-04-01 17:05:54 +0200

    Filled coverage holes in DutySwitch unit tests

--- webinterface/tests/unit_tests/fixtures.py
+++ webinterface/tests/unit_tests/fixtures.py
@@ -1,9 +1,49 @@
 from webinterface.models import *
+import logging
+import sys
+
+
+# class LogThisTestCase(type):
+#     def __new__(cls, name, bases, dct):
+#         # if the TestCase already provides setUp, wrap it
+#         if 'setUp' in dct:
+#             setUp = dct['setUp']
+#         else:
+#             setUp = lambda self: None
+#
+#         def wrappedSetUp(self):
+#             # for hdlr in self.logger.handlers:
+#             #    self.logger.removeHandler(hdlr)
+#             self.hdlr = logging.StreamHandler(sys.stdout)
+#             self.logger.addHandler(self.hdlr)
+#             setUp(self)
+#         dct['setUp'] = wrappedSetUp
+#
+#         # same for tearDown
+#         if 'tearDown' in dct:
+#             tearDown = dct['tearDown']
+#         else:
+#             tearDown = lambda self: None
+#
+#         def wrappedTearDown(self):
+#             tearDown(self)
+#             self.logger.removeHandler(self.hdlr)
+#         dct['tearDown'] = wrappedTearDown
+#
+#         # return the class instance with the replaced setUp/tearDown
+#         return type.__new__(cls, name, bases, dct)
+
+
+# class BaseFixture(metaclass=LogThisTestCase):
+#     logger = logging.getLogger()
+#     logger.setLevel(logging.DEBUG)
 
 
 class BaseFixture:
     @classmethod
     def setUpTestData(cls):
+        logging.getLogger('__name__').setLevel(logging.INFO)
+
         # Config
         cls.start_week = 2500
         cls.mid_week = cls.start_week + 1
--- webinterface/tests/unit_tests/test_model_DutySwitch.py
+++ webinterface/tests/unit_tests/test_model_DutySwitch.py
@@ -47,33 +47,27 @@ class DutySwitchTest(BaseFixtureWithDutySwitch, TestCase):
         self.assertFalse(self.angie_bathroom_dutyswitch_2502.possible_acceptors().exists())
         self.assertFalse(self.dave_garage_dutyswitch_2500.possible_acceptors().exists())
 
+
+class DutySwitchDatabaseTests(TestCase):
+    def setUp(self) -> None:
+        self.schedule = Schedule.objects.create(name='schedule')
+        self.cleaning_week_1 = CleaningWeek.objects.create(week=300, schedule=self.schedule)
+        self.cleaning_week_2 = CleaningWeek.objects.create(week=301, schedule=self.schedule)
+
+        self.cleaner1 = Cleaner.objects.create(name='cleaner1')
+        self.cleaner2 = Cleaner.objects.create(name='cleaner2')
+
+        self.assignment1 = Assignment.objects.create(cleaner=self.cleaner1, cleaning_week=self.cleaning_week_1,
+                                                     schedule=self.schedule)
+        self.assignment2 = Assignment.objects.create(cleaner=self.cleaner2, cleaning_week=self.cleaning_week_2,
+                                                     schedule=self.schedule)
+
     def test__save__acceptor_is_set(self):
-        temp_schedule = Schedule.objects.create(name='temp')
-        cleaning_week_1 = CleaningWeek.objects.create(week=300, schedule=temp_schedule)
-        cleaning_week_2 = CleaningWeek.objects.create(week=301, schedule=temp_schedule)
 
-        temp_cleaner_1 = Cleaner.objects.create(name='temp1')
-        temp_cleaner_2 = Cleaner.objects.create(name='temp2')
-
-        assignment_1 = Assignment.objects.create(cleaner=temp_cleaner_1, cleaning_week=cleaning_week_1,
-                                                 schedule=temp_schedule)
-        assignment_2 = Assignment.objects.create(cleaner=temp_cleaner_2, cleaning_week=cleaning_week_2,
-                                                 schedule=temp_schedule)
-
-        temp_dutyswitch = DutySwitch.objects.create(requester_assignment=assignment_1)
-        temp_dutyswitch.acceptor_assignment = assignment_2
+        temp_dutyswitch = DutySwitch.objects.create(requester_assignment=self.assignment1)
+        temp_dutyswitch.acceptor_assignment = self.assignment2
         temp_dutyswitch.save()
 
-        self.assertEqual(assignment_1.cleaner, temp_cleaner_2)
-        self.assertEqual(assignment_2.cleaner, temp_cleaner_1)
-        self.assertIn(temp_cleaner_1, cleaning_week_1.excluded.all())
-
-        temp_dutyswitch.delete()
-        assignment_1.delete()
-        assignment_2.delete()
-        temp_cleaner_1.delete()
-        temp_cleaner_2.delete()
-        cleaning_week_1.delete()
-        cleaning_week_2.delete()
-        temp_schedule.delete()
-
+        self.assertEqual(self.assignment1.cleaner, self.cleaner2)
+        self.assertEqual(self.assignment2.cleaner, self.cleaner1)
+        self.assertIn(self.cleaner1, self.cleaning_week_1.excluded.all())
